34e779828413a85394b553a61b92ae1b2911c458
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-05-29 15:07:06 +0100

    feature: Frintend UI

---
+++ api/v1/views/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" objects that handle all default RestFul API actions for Tasks """
+
+from models.user import User
+from models.tasks import Tasks
+from models import storage
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+
+@app_views.route('/users', methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/users/<user_id>/', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
---
+++ app/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from models import storage
+from models.user import User
+from models.tasks import Tasks
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def main():
+    user = storage.get(User, id)
+    tasks = user.tasks()
+    return render_template('index.html',
+                           name=name,
+                           tasks=tasks)
+# Research o using external apis in  flask app
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
