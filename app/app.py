8854e79b0b9b4517c82ee4e211c8b3c3aa18d539
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-05-31 16:21:09 +0100

    feature: Create template

--- app/app.py
+++ app/app.py
@@ -3,17 +3,26 @@ from models import storage
 from models.user import User
 from models.tasks import Tasks
 from flask import Flask, render_template
+import os
+
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def main():
-    user = storage.get(User, id)
-    tasks = user.tasks()
+    user = storage.get(User, '00a11245-12fa-436e-9ccc-967417f8c30a')
+    if os.getenv('STORAGE_TYPE', None) == 'db':
+        tasks = user.tasks
+    else:
+        tasks = user.tasks()
+    done = [task for task in tasks if task.completed == 1]
     return render_template('index.html',
-                           name=name,
-                           tasks=tasks)
+                           name=user.first_name,
+                           tasks=tasks, total=len(tasks),
+                           done=len(done))
+
 # Research o using external apis in  flask app
+
 
 if __name__ == "__main__":
     """ Main Function """
