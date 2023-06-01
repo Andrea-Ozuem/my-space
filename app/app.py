1f9223b26cea7600097dc78b0376b7dfee8415f8
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-06-01 19:11:51 +0100

    feat: Add apis for widgets

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+from datetime import datetime
 from models import storage
 from models.user import User
 from models.tasks import Tasks
 from flask import Flask, render_template
+import requests
 import os
 
 
@@ -17,12 +19,35 @@ def main():
     else:
         tasks = user.tasks()
     done = [task for task in tasks if task.completed == 1]
+    description, temp, humidity, pressure, speed, away = get_weather(user)
+
+    now = datetime.now()
+    home = now.strftime("%H:%M")
+
     return render_template('index.html',
                            name=user.first_name,
                            tasks=tasks, total=len(tasks),
-                           done=len(done))
+                           done=len(done), t=temp, humid=humidity, press=pressure, speed=speed, summ=description, home=home, away=away)
 
 # Research o using external apis in  flask app
+
+def get_weather(user):
+    # api = getenv('WEATHER_KEY', None)
+    country = user.country
+    city = user.city
+    api = 'e6073d242a72d509b55819a4e455fb5f'
+    endpoint = 'https://api.openweathermap.org/data/2.5/weather?q={},{}&limit=1&appid={}'.format(city, country, api)
+    r = requests.get(endpoint).json()
+    description = r.get('weather')[0].get('description')
+    temp = r.get('main').get('temp')
+    humidity = r.get('main').get('humidity')
+    pressure = r.get('main').get('pressure')
+    speed = r.get('wind').get('speed')
+
+    time = requests.get('https://timeapi.io/api/Time/current/zone?timeZone=Africa/Lagos').json()
+    time = r.get('time')
+    # do also for home
+    return (description, temp, humidity, pressure, speed, time)
 
 
 if __name__ == "__main__":
--- app/static/main.js
+++ app/static/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-    $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
-        if (data.status === 'OK') { $('div#api_status').addClass('available'); } else { $('div#api_status').removeClass('available');}
-    });
+    let endpoint = 'https://api.linkpreview.net'
+    let apiKey = 'e6073d242a72d509b55819a4e455fb5f'
+    
+    $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/weather?q=' + "?key=" + apiKey + " &q=" + $( this ).text(),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(result){
+            console.log(result);
+        }
+    })
 })
+
+http://api.openweathermap.org/geo/1.0/direct?q=seattle&limit=1&appid=e6073d242a72d509b55819a4e455fb5f
