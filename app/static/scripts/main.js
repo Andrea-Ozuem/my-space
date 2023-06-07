f57cb5c08efa5f177c379964293bedf3cc319f29
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-06-07 08:45:39 +0100

    feature: Edit code

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -2,16 +2,19 @@ $(document).ready(function () {
     $('input[type=checkbox]').change(function() {
         let isChecked = $(this).is(':checked');
         let status = isChecked ? 1 : 0;
+        let task_id = $(this).attr('data-id');
         
         // send request to update task using api
         $.ajax({
-            url: 'http://0.0.0.0:5001/api/v1/tasks/task_id',
+            url: `http://0.0.0.0:5001/api/v1/tasks/${task_id}`,
             type: 'PUT',
-            data: JSON.stringify('completed': status),
+            data: JSON.stringify({'completed': status}),
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
         }).done(function( json ) {
-            console.log(json);
+            let done = parseInt($('span.done').text())
+            done = json.completed ? done + 1 : done - 1
+            $('span.done').text(done)
         })
     });
 });
