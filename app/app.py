791ea9798f86136d1c6ae77cc9ca4a14ffa57811
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-09-07 01:41:42 +0100

    fix: Update requirements.txt

--- app/app.py
+++ app/app.py
@@ -232,5 +232,4 @@ def get_weather(user):
 
 
 if __name__ == "__main__":
-    """ Main Function """
     app.run(host='0.0.0.0', port=5000, debug=True)
--- tests/test_models/test_base.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import unittest
-
-'''Tests for BaseMOdel and all inherieted properties for app classes'''
-
-class Unittest():
